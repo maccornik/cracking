0bb39682a70ef3a678f4de4c6171aa155ccaf16c
Bartłomiej Struzik <maccornik@gmail.com>
2018-04-01 01:15:40 +0200

    MyStringBuffer list implementation.

--- strings/src/cci/stringbuffer/MyStringBuffer.java
+++ strings/src/cci/stringbuffer/MyStringBuffer.java
@@ -1,4 +1,37 @@
 package cci.stringbuffer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyStringBuffer {
+    private final List<String> strings;
+
+    public MyStringBuffer(List<String> strings) {
+        this.strings = strings;
+    }
+
+    public MyStringBuffer() {
+        this.strings = new ArrayList<>();
+    }
+
+    public void append(String s) {
+        strings.add(s);
+    }
+
+    @Override
+    public String toString() {
+        String concatenated = "";
+        for (String s : strings) {
+            concatenated = concatenated + s;
+        }
+        return concatenated;
+    }
+
+    public static void main(String[] args) {
+        MyStringBuffer buffer = new MyStringBuffer();
+        buffer.append("jeden");
+        buffer.append(" ");
+        buffer.append("dwa");
+        System.out.println(buffer);
+    }
 }
